0e2ba453ad68bb1317f35c41d472f6909badec4b
liupeng <361141665@qq.com>
2017-12-13 18:21:19 +0800

    继续测试冲突

--- src/test/java/com/zheng/SpringbootApplicationTests.java
+++ src/test/java/com/zheng/SpringbootApplicationTests.java
@@ -33,7 +33,7 @@ public class SpringbootApplicationTests {
 		userMapper.insert("wangwu", 3);
 		userMapper.insert("zhaoliu", 4);
         System.out.print("gitTest测试冲突");
-		User user = userMapper.findByName("lisi测试");
+		User user = userMapper.findByName("lisi");
 		System.out.println(user.getId());
 
 	}
